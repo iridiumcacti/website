2313e0f1cbe230eb406b26c2855ce0856d3ab7b2
Carlos Rivera <me@carlosrivera.im>
2021-08-13 00:23:20 -0500

    initial commit 🚀

--- pages/index.js
+++ pages/index.js
@@ -2,81 +2,64 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+    <div className="bg-indigo-900 relative overflow-hidden h-screen">
+      <img src="/background.svg" className="absolute h-full w-full object-cover" />
+      <div className="inset-0 bg-black opacity-25 absolute">
+      </div>
+      <header className="absolute top-0 left-0 right-0 z-20">
+        <nav className="container mx-auto px-6 md:px-12 py-4">
+          <div className="md:flex justify-between items-center">
+            <div className="flex justify-between items-center">
+              <a href="#" className="text-white">
+                <svg className="w-8 mr-2 fill-current" xmlns="http://www.w3.org/2000/svg" data-name="Capa 1" viewBox="0 0 16.16 12.57">
+                  <defs>
+                  </defs>
+                  <path d="M14.02 4.77v7.8H9.33V8.8h-2.5v3.77H2.14v-7.8h11.88z">
+                  </path>
+                  <path d="M16.16 5.82H0L8.08 0l8.08 5.82z">
+                  </path>
+                </svg>
+              </a>
+              <div className="md:hidden">
+                <button className="text-white focus:outline-none">
+                  <svg className="h-12 w-12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4 6H20M4 12H20M4 18H20" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    </path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div className="font-extrabold hidden md:flex items-center">
+              <a className="text-lg uppercase mx-3 text-white cursor-pointer hover:text-gray-300">
+                About us
+              </a>
+              <a className="text-lg uppercase mx-3 text-white cursor-pointer hover:text-gray-300">
+                Contact us
+              </a>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-48">
+        <div className="lg:w-4/5 xl:w-3/5 flex flex-col items-start relative z-10">
+          <p>
+            <span className="font-extrabold uppercase text-yellow-400">
+              Iridium
+            </span>
+            <span className="font-light uppercase text-yellow-400">
+              Cacti
+            </span>
+          </p>
+          <h1 className="font-bold text-6xl sm:text-7xl text-white leading-tight mt-4">
+            Let us help you to
+            <br />
+            sell online.
+          </h1>
+          <a href="#" className="block bg-white hover:bg-gray-100 py-3 px-4 rounded-lg text-lg text-gray-800 font-bold uppercase mt-10">
+            Discover how
           </a>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      </div>
     </div>
   )
 }
